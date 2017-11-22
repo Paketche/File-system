b51183ddfe4a8ab8f1859ca6bdefd8a0a3ed3386
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2017-11-22 00:10:54 +0000

    Update Volume class
    Added comments to all init methods, added a getInode method, fixed the
    calculation of groupDescriptor and the inode offset in the initInode
    table.

--- File System/src/fylesys/Volume.java
+++ File System/src/fylesys/Volume.java
@@ -18,45 +18,51 @@ public class Volume {
 		file = new RandomAccessFile(path, "r");
 		byte[] full = new byte[(int) file.length()];
 		file.readFully(full);
-		buffer = new LittleEndianBuffer(full, 1024);	
+		buffer = new LittleEndianBuffer(full, 1024);
 
 		initSuperBlock();
 		initGroupDescriptors();
 		initInodeTable();
 	}
 
-
-	private void initSuperBlock(){
+	/**
+	 * Initializes a copy a super block
+	 */
+	private void initSuperBlock() {
 		// get the block that is just after the boot block
 		superBlock = new SuperBlock(buffer.readBlock(blockSize));
 	}
 
-	private void initGroupDescriptors(){
+	/**
+	 * Initializes an array of Group Descriptors
+	 */
+	private void initGroupDescriptors() {
 		// gets the number of group descriptors
 		// if the division has a remainder we add 1 to round it up
 		int gds = superBlock.getInodeCount() / superBlock.getInodesPerGroup()
 				+ (superBlock.getInodeCount() % superBlock.getInodesPerGroup() == 0 ? 0 : 1);
-		
+
 		groupDescriptors = new GroupDescriptor[gds];
 
 		for (int i = 0; i < groupDescriptors.length; i++) {
-			// reads 32 bytes into the descriptor
-			// i is used as an offset
-			groupDescriptors[i] = new GroupDescriptor(buffer.read(2048 + 32 * i, 32));
+			// Read data of size equal to a group descriptors size,
+			// i * group descriptor's size is used as an offset
+			// start reading from the second block where the first group descriptor is
+			groupDescriptors[i] = new GroupDescriptor(buffer.read(2 * blockSize + i * GroupDescriptor.size, GroupDescriptor.size));
 		}
 	}
 
-	private void initInodeTable(){
+	/**
+	 * Initializes an array of all the inodes
+	 */
+	private void initInodeTable() {
 
 		inodeTable = new Inode[superBlock.getInodeCount()];
 
 		// filles in the inode table
 		for (int i = 0; i < inodeTable.length; i++) {
 			/*
-			 * 
-			 * 
-			 * 
-			 * get the offset of the inode in the table:
+			 * get the offset from the begging of an inode table:
 			 * 
 			 * say we want inode 5 and there are 3 inodes per group block this variable will
 			 * then be 1 because inode 5 will be the second inode in the second block group
@@ -64,17 +70,29 @@ public class Volume {
 			int inode = i % superBlock.getInodesPerGroup();
 
 			/*
-			 * index of the group descriptor that the inode will be
-			 * 
-			 * say we want inode 6 and there are 3 inodes per group giving us 2; in cases
-			 * where the division has no remainder remove 1 -> 6/3 = 2 -> 2-1 = 1 (1 is the
-			 * index of the second group descriptor)
+			 * index pointing to a group descriptor int the group descriptor array that the
+			 * inode will be in
 			 */
-			int groupDescritor = i / superBlock.getInodesPerGroup();// + (i % superBlock.getInodesPerGroup() == 0 && i != 0 ? -1 : 0);
+			int groupDescritor = i / superBlock.getInodesPerGroup();
 
-			// get the inode table pointer and read the inode that was calculated in the
-			// "inode" variable (inodes are 128 bytes)
-			inodeTable[i] = new Inode(buffer.read(groupDescriptors[groupDescritor].getInodeTablePointer() + inode * 128, 128));
+			// get the inode table pointer
+			inodeTable[i] = new Inode(
+					buffer.read(groupDescriptors[groupDescritor].getInodeTablePointer() * blockSize + inode * Inode.size, Inode.size));
 		}
+	}
+
+	/**
+	 * Retrieves an Inode
+	 * 
+	 * @param inodeNumber
+	 *          id number of the inode
+	 * @return specified inode
+	 */
+	Inode getInode(int inodeNumber) {
+		if (inodeNumber < 1) {
+			throw new NullPointerException();
+		}
+
+		return inodeTable[inodeNumber - 1];
 	}
 }
