6711df5af355f385e9aa09ac306e7e409b665451
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2017-11-28 21:09:57 +0000

    Modify SuperBlock.java
    Instead of reading from the volume's buffer every time now every thing
    is saved into private fields

--- File System/src/fs2/SuperBlock.java
+++ File System/src/fs2/SuperBlock.java
@@ -1,43 +1,111 @@
 package fs2;
 
 public class SuperBlock {
-
-	private Volume volume;
-	private int offset;
-
+	/*
+	 * Offsets
+	 */
 	private static final int numInodesFileSys = 0;
 	private static final int numBlocksFileSys = 4;
 	private static final int numBlocksGroup = 32;
 	private static final int numInodesGroup = 40;
 	private static final int inodeSize = 88;
-	private static final int volLable = 120;
+	private static final int volLabel = 120;
 
-	public SuperBlock(Volume vol, int offset) {
-		volume = vol;
-		this.offset = offset;
+	/**
+	 * The number of inodes in the file system
+	 */
+	private final int inodefs;
+	/**
+	 * The number of data blocks in the file system
+	 */
+	private final int blocksfs;
+	/**
+	 * The number of blocks per block group
+	 */
+	private final int blocksgr;
+	/**
+	 * The number of inodes per block group
+	 */
+	private final int inodesgr;
+	/**
+	 * Size of an inode in bytes
+	 */
+	private final int inodesz;
+	/**
+	 * Label of the volume
+	 */
+	private final String label;
+
+	/**
+	 * Creates Super Block data block
+	 * 
+	 * @param vol
+	 *          volume where The super block data is written
+	 * @param offset
+	 *          the byte from which the data of the Super Block is written on the
+	 *          volume
+	 */
+	public SuperBlock(Volume volume, int offset) {
+		
+		inodefs = volume.getIntAt(offset + numInodesFileSys, 4);
+		blocksfs = volume.getIntAt(offset + numBlocksFileSys, 4);
+		blocksgr = volume.getIntAt(offset + numBlocksGroup, 4);
+		inodesgr = volume.getIntAt(offset + numInodesGroup, 4);
+		inodesz = volume.getIntAt(offset + inodeSize, 4);
+		label = volume.getStringAt(offset + volLabel, 16);
 	}
 
+	/**
+	 * Returns the number of inodes in the file system
+	 * 
+	 * @return number of inodes in the file system
+	 */
 	public int inodesInFileSystem() {
-		return volume.getIntAt(offset + numInodesFileSys, 4);
+		return inodefs;
 	}
 
+	/**
+	 * Returns the number of data blocks in the file system
+	 * 
+	 * @return number of data blocks in the file system
+	 */
 	public int blocksInFileSystem() {
-		return volume.getIntAt(offset + numBlocksFileSys, 4);
+		return blocksfs;
 	}
 
+	/**
+	 * Returns the number of blocks per block group
+	 * 
+	 * @return the number of blocks per block group
+	 */
 	public int blocksInGroups() {
-		return volume.getIntAt(offset + numBlocksGroup, 4);
+		return blocksgr;
 	}
 
+	/**
+	 * Returns the number of inodes per block group
+	 * 
+	 * @return the number of inodes per block group
+	 */
 	public int inodesInGroup() {
-		return volume.getIntAt(offset + numInodesGroup, 4);
+		return inodesgr;
 	}
 
+	/**
+	 * Returns the size of an inode in bytes
+	 * 
+	 * @return size of an inode in bytes
+	 */
 	public int inodeSize() {
-		return volume.getIntAt(offset + inodeSize, 4);
+		return inodesz;
 	}
 
+	/**
+	 * Returns the label of the volume
+	 * 
+	 * @return the label of the volume
+	 */
 	public String volumeName() {
-		return volume.getStringAt(offset + volLable, 16);
+		return label;
 	}
 }
